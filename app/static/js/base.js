ab6f8702f72fb4e1dcbc50533a90fc8ef92328fd
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-03-12 22:26:58 +0200

    fix logout button

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -3,24 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   function Logout(event) {
       event.preventDefault();
 
-      console.log('test1')
-
       fetch('/logout', {method: "POST"})
       .then(() => {
         window.location.href = '/';
-        console.log('test2')
       });
     }
 
 
   elSignOutDesktop = document.getElementById('sign-out-desktop');
-  console.log(elSignOutDesktop)
   if (elSignOutDesktop) {
      elSignOutDesktop.addEventListener('click', Logout);
   }
 
   elSignOutMobility = document.getElementById('sign-out-mobility');
-  console.log(elSignOutMobility)
   if (elSignOutMobility) {
      elSignOutMobility.addEventListener('click', Logout)
   }
